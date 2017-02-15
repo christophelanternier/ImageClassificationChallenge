05d151a5e653af009a79485f5b7884ccdacb7d68
Louis THIRY <louis.thiry@outlook.fr>
2017-02-15 14:09:36 +0100

    test logistic regression classifier on raw data

--- code/classification.py
+++ code/classification.py
@@ -1,18 +1,17 @@
 import cvxopt
 import numpy as np
 
+def sigma(u):
+    return 1.0 / (1 + np.exp(-u))
+
 def logistic_loss(f_x, y):
     return np.log(1 + np.exp(-y * f_x))
 
 def logistic_loss_prime(f_x, y):
     return - 1.0 / (1 + np.exp(y * f_x))
 
-def logistic_regression_loss_function(alpha, f_values, labels, _lambda):
+def logistic_regression_loss_function(alpha, K, labels, _lambda):
     N = len(alpha)
-    K = np.zeros((N,N))
-    for i in range(0, N):
-        for j in range(0, N):
-            K[i, j] = f_values[i,:].dot(f_values[j,:])
     f_x = K.dot(alpha)
     result = 0
 
@@ -26,12 +25,8 @@ def logistic_regression_loss_function(alpha, f_values, labels, _lambda):
 
     return result
 
-def grad_logistic_regression_loss_function(alpha, f_values, labels, _lambda):
+def grad_logistic_regression_loss_function(alpha, K, labels, _lambda):
     N = len(alpha)
-    K = np.zeros((N,N))
-    for i in range(0, N):
-        for j in range(0, N):
-            K[i, j] = f_values[i,:].dot(f_values[j,:])
     P_diag = np.zeros(N)
     f_x = K.dot(alpha)
 
@@ -53,27 +48,35 @@ def compute_K(f_values):
 def logistic_regression_classifier(f_values, labels, _lambda=0):
     # kernel logistic regression solved by gradient descent
     N = len(f_values[:,0])
+    K = compute_K(f_values)
+    alpha_0 = np.zeros(N)
 
     default_step = 10000
-    alpha_0 = np.zeros(N)
-    stop_criterion = 10**(-3)
+    stop_criterion = 10**(-1)
     gradient_norm = stop_criterion + 1
-    n_iteration_max = 500
+    n_iteration_max = 300
     iter = 0
 
     while (iter < n_iteration_max) and (gradient_norm > stop_criterion):
         step = default_step
-        L = logistic_regression_loss_function(alpha_0, f_values, labels, _lambda)
-        grad_L = grad_logistic_regression_loss_function(alpha_0, f_values, labels, _lambda)
-        gradient_norm = np.linalg.norm(grad_L)
-        L_update = logistic_regression_loss_function(alpha_0 - step * grad_L, f_values, labels, _lambda)
+        L = logistic_regression_loss_function(alpha_0, K, labels, _lambda)
+        grad_L = grad_logistic_regression_loss_function(alpha_0, K, labels, _lambda)
+        gradient_norm = np.linalg.norm(grad_L) / len(grad_L)
+        L_update = logistic_regression_loss_function(alpha_0 - step * grad_L, K, labels, _lambda)
         while (L_update > L):
             step *= 0.5
-            L_update = logistic_regression_loss_function(alpha_0 - step * grad_L, f_values, labels, _lambda)
+            L_update = logistic_regression_loss_function(alpha_0 - step * grad_L, K, labels, _lambda)
 
         alpha_0 = alpha_0 - step * grad_L
         iter +=1
 
-        print "iter ", iter, " alpha ", alpha_0
-
+    if (iter == n_iteration_max):
+        print "max iteration number reached"
     return alpha_0
+
+def predict(alpha, f_values, x):
+    f_x  = 0
+    for i in range(0, len(alpha)):
+        f_x += alpha[i] * f_values[i].dot(x)
+
+    return f_x
--- code/test.py
+++ code/test.py
@@ -1,7 +1,7 @@
 from classification import *
 import numpy as np
 
-n_points = 50
+n_points = 10
 points = np.random.rand(n_points, 2)
 w = np.array([0.5, -0.5])
 
@@ -14,8 +14,5 @@ alpha = logistic_regression_classifier(points, labels)
 
 print(alpha)
 
-K = compute_K(points)
-f_x = K.dot(alpha)
-
 for i in range(0, n_points):
-    print "true label ", labels[i], " pred label ", 2 * int(f_x[i] > 0) - 1
+    print "true label ", labels[i], " pred label ", predict(alpha, points, points[i,:])
