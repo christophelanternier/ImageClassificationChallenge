67ef2b9691c8758e97a0c9479733f57145059652
Louis THIRY <louis.thiry@outlook.fr>
2017-02-16 20:06:44 +0100

    create file for kernels

--- code/kernel.py
+++ code/kernel.py
@@ -1,9 +1,29 @@
 import numpy as np
 
-def fourier_kernel(images):
-    fourier_features = np.zeros_like(images)
+def fourier_1D_kernel(images):
+    """
+        simply returns the fourier transform of the image seen as a 1D array
+    """
+    fourier_1D_features = np.zeros_like(images)
 
     for i in range(images.shape[0]):
-        fourier_features[i,:] = np.abs(np.fft.fft(images[i,:]))
+        fourier_1D_features[i,:] = np.abs(np.fft.fft(images[i,:]))
 
-    return fourier_features
+    return fourier_1D_features
+
+def fourier_2D_kernel(images):
+    """
+        returns 2Dthe fourier transform of the image seen
+    """
+    fourier_2D_features = np.zeros_like(images)
+
+    for i in range(images.shape[0]):
+        image_1 = images[i,0:1024].reshape((32, 32))
+        image_2 = images[i,1024:2048].reshape((32, 32))
+        image_3 = images[i,2048:3072].reshape((32, 32))
+
+        fourier_2D_features[i,0:1024] = np.abs(np.fft.fft2(image_1)).reshape(1024)
+        fourier_2D_features[i,1024:2048] = np.abs(np.fft.fft2(image_2)).reshape(1024)
+        fourier_2D_features[i,2048:3072] = np.abs(np.fft.fft2(image_3)).reshape(1024)
+
+    return fourier_2D_features
