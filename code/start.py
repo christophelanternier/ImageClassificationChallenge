da1bede224e7af8bcbae94f30306fed7b1aa76ef
Louis THIRY <louis.thiry@outlook.fr>
2017-02-21 18:57:10 +0100

    test fourier 1D features

--- code/start.py
+++ code/start.py
@@ -1,32 +1,24 @@
 import numpy as np
 from numpy import genfromtxt
 from classification import *
+from kernel import *
 
 # store data
 Xtr = genfromtxt('../data/Xtr.csv', delimiter=',')
 Ytr = genfromtxt('../data/Ytr.csv', delimiter=',')
+Xte = genfromtxt('../data/Xte.csv', delimiter=',')
+
 Xtr = np.delete(Xtr, 3072, axis=1)
+Xte = np.delete(Xte, 3072, axis=1)
 Ytr = Ytr[1:,1]
 N = len(Ytr)
-classes = [int(c) for c in set(Ytr)]
-n_classes = len(classes)
 
-# divide data in train and test for validation
-N_train = 2 * N / 3
-N_test = N - N_train
 
-train_images = Xtr[:N_train]
-train_labels = Ytr[:N_train]
+train_images = fourier_1D_kernel(Xtr)
+train_labels = Ytr
 
-test_images = Xtr[N_train:N]
-test_labels = Ytr[N_train:N]
-
+test_images = fourier_1D_kernel(Xte)
 
 alphas = train_one_versus_all_logistic_classifier(train_images, train_labels)
 n_labels = 10
-test_one_versus_all_logistic_classifier(alphas, train_images, test_images, n_labels, test_labels)
-
-
-
-
-
+test_one_versus_all_logistic_classifier(alphas, train_images, test_images, n_labels, test_labels=None, filename='../data/Yte_fourier_1D_log_reg.csv')
