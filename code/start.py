b606a8221ec2482db420282088dcd5eedce85bee
Louis THIRY <louis.thiry@outlook.fr>
2017-02-25 12:00:44 +0100

    cross validation of scattering kernel

--- code/start.py
+++ code/start.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import genfromtxt
 from classification import *
 from kernel import *
+from datetime import time, datetime, timedelta
 
 # store data
 Xtr = genfromtxt('../data/Xtr.csv', delimiter=',')
@@ -13,17 +14,36 @@ Xte = np.delete(Xte, 3072, axis=1)
 Ytr = Ytr[1:,1]
 N = len(Ytr)
 
-#train_images = fourier_1D_kernel(Xtr)
-train_images = wavelet_transform(Xtr)
-train_labels = Ytr
+N_train = 3000
+train_images = Xtr[0:N_train,:]
+train_labels = Ytr[0:N_train,:]
 
-test_images = wavelet_transform(Xte)
+test_images = Xtr[N_train:,:]
+test_labels = Ytr[N_train:,:]
 
-train_images = fourier_modulus_1D_kernel(Xtr)
-train_labels = Ytr
+maximum_scales = [2, 3, 4]
+lambdas = [0.1, 0.01, 0.001]
 
-test_images = fourier_modulus_1D_kernel(Xte)
+for maximum_scale in maximum_scales:
+    train_scattering_features = scattering_kernel(train_images, maximum_scale=maximum_scale).T
+    test_scattering_features = scattering_kernel(test_images, maximum_scale=maximum_scale).T
 
-alphas = train_one_versus_all_logistic_classifier(train_images, train_labels)
-n_labels = 10
-test_one_versus_all_logistic_classifier(alphas, train_images, test_images, n_labels, test_labels=None, filename='../data/Yte_fourier_mod_1D_log_reg.csv')
+    for _lambda in lambdas:
+        t1 = datetime.now()
+        alphas, bias = one_versus_all_SVM(train_scattering_features, train_labels, _lambda=_lambda)
+        t2 = datetime.now()
+        print 'model fitted. duration: ', t2 - t1
+        prediction = predict_SVM(alphas, bias, train_scattering_features, test_scattering_features)
+        t3 = datetime.now()
+        print 'prediction done. duration: ', t3 - t2
+
+        if test_labels is not None:
+            well_classified = 0
+            for i in range(len(prediction)):
+                if prediction[i] == test_labels[i]:
+                    well_classified += 1
+                print 'lambda = ', _lambda, ', good classification rate = ', float(well_classified) / (N - N_train)
+        else:
+            DF = pd.DataFrame(data=pd.Series(prediction), columns=['Prediction'])
+            DF.index += 1
+            DF.to_csv('../data/'+'scattering_transform_lambda_' + str(_lambda) + '.csv', index=True, index_label='Id', sep=',')
